c0e9e1aa2d113eea87eaf65148815715849cb5bf
joshi-chirag <kcchirag27@gmail.com>
2026-05-23 12:00:00 +0530

    feat: implement secure JWT authentication and profile endpoints

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password2 = serializers.CharField(write_only=True, label='Confirm Password')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    dag_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'date_joined', 'dag_count']
+
+    def get_dag_count(self, obj):
+        return obj.dags.count()
