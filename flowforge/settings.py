93477139ac35cf47cd4286ce2ff1d30a4830c55d
joshi-chirag <kcchirag27@gmail.com>
2026-05-30 12:00:10 +0530

    fix: use CompressedStaticFilesStorage to serve unhashed static files

--- flowforge/settings.py
+++ flowforge/settings.py
@@ -172,7 +172,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
